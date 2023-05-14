2dfcc1d70ed89a341e074b023ca4b0537850283d
Maina Ngaruiya <100405059+mainangaruiya@users.noreply.github.com>
2023-05-14 22:41:22 +0300

    the clone

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""
-Module for Amenity class
-"""
+"""Defines the Amenity class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """Custom amenity class
+class Amenity(BaseModel, Base):
+    """Represents an Amenity for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
 
     Attributes:
-        name(str): amenity name
-
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
